8993c31d6140436f86ba9b983e286a03d1df8a4d
BRAVO PEREZ <227019@alumnos.uia>
2023-09-14 16:06:17 -0600

    24.2) Se hizo un programa de javascript para poder retomar el concepto de los valores primitivos y hacer progeramas como en C.Por otro lado, los objetos en JS existen y son similares a las estructuras en C.

---
+++ HTML_CSS_JS_BOOSTRAP/comportamiento_de_documento/declarar_variables/tipos_variables.js
@@ -0,0 +1,26 @@
+var x;
+x=3.1416;
+
+function main() {
+    console.clear();
+    if (x == undefined)
+    {
+        console.log("\n\n\n\nLa variable x no está definida :(.\n\n");
+    }
+    else
+    {
+        console.log("\n\n\n\nEl valor de X es: " + x);
+    }
+    
+}
+
+
+
+
+/** EJECUCIÓN DE FUNCIONES -------
+ *                               |
+ *                               |
+ *                               |
+ *                               V    */
+
+main();
