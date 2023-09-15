55a48fe8aba4b9a865d213bb506d53c45720247d
BRAVO PEREZ <227019@alumnos.uia>
2023-09-15 09:31:28 -0600

    Se hizo un pequeño experimento en el programa de 'tipos_variables.js'.

--- HTML_CSS_JS_BOOSTRAP/comportamiento_de_documento/declarar_variables/tipos_variables.js
+++ HTML_CSS_JS_BOOSTRAP/comportamiento_de_documento/declarar_variables/tipos_variables.js
@@ -5,11 +5,18 @@ function main() {
     console.clear();
     if (x == undefined)
     {
-        console.log("\n\n\n\nLa variable x no está definida :(.\n\n");
+        console.log("\n\n\nLa variable x no está definida :(.\n\n");
+    } 
+
+    if(x==3.1416)
+    {
+        x=x+2.7182;
+        console.log("\n\n\nAhora me sumé con el número de euler y mi resultado es: " + x + "\n\n\n\n");
     }
+    
     else
     {
-        console.log("\n\n\n\nEl valor de X es: " + x);
+        console.log("\n\n\nEl valor de X es: " + x + "\n\n\n\n");
     }
     
 }
