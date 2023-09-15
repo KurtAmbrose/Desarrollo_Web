9427f0167c0d015ba1d5e8077b62661990c05768
BRAVO PEREZ <227019@alumnos.uia>
2023-09-15 14:49:10 -0600

    26.2) CREACIPON DE OBJETOS: Se mostró una manera mucho más sencilla de declarar los objetos y es muy similar a la de C. Así de que de ahora en adelante ocuparé esa misma.

--- HTML_CSS_JS_BOOSTRAP/comportamiento_de_documento/creacion_objetos/creacionObjetos.js
+++ HTML_CSS_JS_BOOSTRAP/comportamiento_de_documento/creacion_objetos/creacionObjetos.js
@@ -13,10 +13,25 @@ compania[pais]="Tierra";
 
 
 console.clear();
-
 console.log(compania);
 console.log("\n\nEl jefe de la compañía " + compania["nombre"] + " es ---> " + compania.ceo.nombre + "\n" + "El valor de la compañia es de " + compania["valor de la compañía"] + "\n\n");
 console.log(compania["nombre"]);
 console.log(compania[pais]);
 
+console.log("\n\n"); 
+
+/** ----------- MEJOR MANERA DE DECLARAR LOS OBJETOS ------------------ */
+
+const comp2 = {
+    "nombre de la compañía": "Tiktok",
+    CEO: {
+        nombre: "Shou",
+        "apellido paterno": "Zi",
+        "apellido materno": "Chew",
+        colorFav: "Negro y Blanco"
+    },
+    "valor de la compañía": 300
+};
+
+console.log(comp2);
 console.log("\n\n");
